6acb417ed5c0887abcad0a3d1d298a26d3e4a068
Jonathan Sönnerup <Nooxet@gmail.com>
2021-02-20 23:39:13 +0100

    Use HW fading with interrupts, wip

--- src/ledrock-fw.c
+++ src/ledrock-fw.c
@@ -4,10 +4,32 @@
 #include "driver/ledc.h"
 #include "esp_err.h"
 
-#define LEDC_CLK_FREQ   5000
-#define LEDC_IO_RED     16
-#define LEDC_IO_GREEN   17
-#define LEDC_IO_BLUE    18
+#define LEDC_CLK_FREQ 5000
+#define LEDC_IO_RED 16
+#define LEDC_IO_GREEN 17
+#define LEDC_IO_BLUE 18
+
+typedef enum
+{
+    RED,
+    GREEN,
+    BLUE
+} ledcolor;
+
+
+volatile static int test = 0;
+
+
+void set_led(ledc_channel_config_t *led_conf, int duty)
+{
+    ledc_set_duty(led_conf->speed_mode, led_conf->channel, duty);
+    ledc_update_duty(led_conf->speed_mode, led_conf->channel);
+}
+
+void IRAM_ATTR led_isr_handler(void *arg)
+{
+    test = 1;
+}
 
 void app_main(void)
 {
@@ -20,32 +42,69 @@ void app_main(void)
         .clk_cfg = LEDC_AUTO_CLK
     };
     ledc_timer_config(&timer_conf);
-    
+
     // Set up LED channels
-    ledc_channel_config_t led_conf = {
-        .gpio_num = LEDC_IO_RED,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_0,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0
+    ledc_channel_config_t led_conf[3] = {
+        {
+            .gpio_num = LEDC_IO_RED,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = LEDC_CHANNEL_0,
+            .timer_sel = LEDC_TIMER_0,
+            .duty = 0,
+            .intr_type = LEDC_INTR_FADE_END
+        },
+        {
+            .gpio_num = LEDC_IO_GREEN,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = LEDC_CHANNEL_1,
+            .timer_sel = LEDC_TIMER_0,
+            .duty = 0,
+            .intr_type = LEDC_INTR_FADE_END
+        },
+        {
+            .gpio_num = LEDC_IO_BLUE,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = LEDC_CHANNEL_2,
+            .timer_sel = LEDC_TIMER_0,
+            .duty = 0
+        }
     };
-    ledc_channel_config(&led_conf);
+
+    for (int i = 0; i < 3; i++)
+    {
+        ledc_channel_config(&led_conf[i]);
+    }
+
+    /**
+     * Enable HW fade and register interrupt.
+     * Note that ledc_fade_func_install must come first, and also the SHARED flag needs to be set (docs sucks)
+     */
+    ESP_ERROR_CHECK(ledc_fade_func_install(ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED));
+    ESP_ERROR_CHECK(ledc_isr_register(led_isr_handler, NULL, ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED, NULL));
+
+    ledc_set_fade_with_time(led_conf[RED].speed_mode, led_conf[RED].channel, 1023, 5000);
+    ledc_fade_start(led_conf[RED].speed_mode, led_conf[RED].channel, LEDC_FADE_NO_WAIT);
 
     while (1)
     {
-        printf("Setting duty = 256\n");
-        ledc_set_duty(led_conf.speed_mode, led_conf.channel, 256);
-        ledc_update_duty(led_conf.speed_mode, led_conf.channel);
+        if (test) {
+            printf("INTERRUPT TRIGGED!\n");
+        }
+        /*
+        printf("Setting red duty = 256\n");
+        set_led(&led_conf[RED], 256);
+        set_led(&led_conf[GREEN], 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        printf("Setting duty = 512\n");
-        ledc_set_duty(led_conf.speed_mode, led_conf.channel, 512);
-        ledc_update_duty(led_conf.speed_mode, led_conf.channel);
+        printf("Setting green duty = 512\n");
+        set_led(&led_conf[RED], 0);
+        set_led(&led_conf[GREEN], 512);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        printf("Setting duty = 1023\n");
-        ledc_set_duty(led_conf.speed_mode, led_conf.channel, 1023);
-        ledc_update_duty(led_conf.speed_mode, led_conf.channel);
+        printf("Setting g/r duty = 256\n");
+        set_led(&led_conf[RED], 256);
+        set_led(&led_conf[GREEN], 256);
+        */
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
