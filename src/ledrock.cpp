06ca28b6f472b52aa21a0c3392a060437dcb30d6
Jonathan Sönnerup <nooxet@gmail.com>
2023-01-07 00:51:09 +0100

    Fixed interrupt bug

--- src/ledrock.cpp
+++ src/ledrock.cpp
@@ -8,11 +8,14 @@
 #define LEDC_CLK_FREQ 5000
 #define LEDC_IO_RED 16
 #define LEDC_IO_GREEN 17
-#define LEDC_IO_BLUE 18
+#define LEDC_IO_BLUE 5 // 18
+
+#define BTN GPIO_NUM_18
 
 extern "C"
 {
     void app_main();
+    void IRAM_ATTR gpio_isr_handler(void *arg);
 }
 
 typedef enum
@@ -40,7 +43,7 @@ void IRAM_ATTR led_isr_handler(void *arg)
 
 void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    printf("GOT INPUT INTERRUPT!");
+    test = 1;
 }
 
 void app_main(void)
@@ -103,16 +106,16 @@ void app_main(void)
 
     gpio_config_t btnConfig =
     {
-        .pin_bit_mask = 1ULL << GPIO_NUM_19,
+        .pin_bit_mask = 1ULL << BTN,
         .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_ENABLE,
-        .intr_type = GPIO_INTR_POSEDGE
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_NEGEDGE
     };
 
     gpio_config(&btnConfig);
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(GPIO_NUM_19, gpio_isr_handler, NULL);
+    gpio_isr_handler_add(BTN, gpio_isr_handler, NULL);
 
     while (1)
     {
