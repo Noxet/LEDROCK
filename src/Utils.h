2b23a87764685e0298f500fa477e0b23baf1c78b
Jonathan Sönnerup <nooxet@gmail.com>
2025-05-10 00:58:13 +0200

    Total rewrite, started from scratch with latest IDF

--- src/Utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Hardware/LED.h"
-
-LED &getLed();
--- src/ledrock.cpp
+++ src/ledrock.cpp
@@ -1,186 +1,121 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "driver/ledc.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
 #include "driver/gpio.h"
-#include "esp_err.h"
+#include "driver/ledc.h"
+#include "hal/ledc_types.h"
+#include "portmacro.h"
+#include "soc/clk_tree_defs.h"
 
+#include <atomic>
+#include <stdio.h>
+#include <inttypes.h>
 
-#include <list>
-#include <functional>
-#include <memory>
+constexpr gpio_num_t LEDPIN = GPIO_NUM_16;
 
-#include "Events/ButtonEvent.h"
-#include "Events/TimerEvent.h"
-#include "Events/LEDEvent.h"
-#include "ColorManager.h"
-#include "Colors/ColorMode.h"
-#include "Colors/ColorUtils.h"
-#include "Utils.h"
+std::atomic<int> count = 0;
 
-
-
-#define BTN GPIO_NUM_18
-
-
-extern "C"
+static  bool cb_ledc_fade_end_event(const ledc_cb_param_t *param, void *user_arg)
 {
-    void app_main();
-    void IRAM_ATTR gpio_isr_handler(void *arg);
-    bool IRAM_ATTR timer_isr_handler(void *arg);
-    bool IRAM_ATTR debounce_timer_handler(void *arg);
-    bool IRAM_ATTR led_fade_handler(const ledc_cb_param_t *param, void *arg);
-}
-
-
-typedef std::list<std::function<void()>> EventHandler;
-EventHandler eventHandler;
-
-ColorManager g_colorManager;
-
-Timer g_debounceTimer(20, debounce_timer_handler, false, TIMER_0);
-static bool g_debounceTimerRunning { false };
-
-static int g_fadeCompleteCount = 0;
-
-
-void IRAM_ATTR gpio_isr_handler(void *arg)
-{
-    ets_printf("Button Pressed ISR Handler\n");
-    // timer already in progress, wait until finished before starting another one
-    if (g_debounceTimerRunning) return;
-    
-    g_debounceTimerRunning = true;
-    g_debounceTimer.restart();
-}
-
-
-bool IRAM_ATTR debounce_timer_handler(void *arg)
-{
-    // timer has stopped and generated called this function
-    g_debounceTimerRunning = false;
-
-    // Button has pull-up, so it is pressed when value = 0
-    if (gpio_get_level(BTN) == 1) return false;
-
-    // detected button press after debouncing
-    ButtonPressedEvent ev(0);
-
-    /*
-    * Here, we have to take the event by copy, since the variable goes out of scope
-    * after this call. We also need to make the capture list mutable, due to the downcasting
-    * in subsequent function calls.
-    */
-    eventHandler.push_back([ev]() mutable { g_colorManager.onEvent(&ev); });
-    return false;
-}
-
-
-bool IRAM_ATTR timer_isr_handler(void *arg)
-{
-    TimerAlarmEvent ev(1);
-    eventHandler.push_back([ev]() mutable { g_colorManager.onEvent(&ev); });
-    return false;
-}
-
-
-bool IRAM_ATTR led_fade_handler(const ledc_cb_param_t *param, void *arg)
-{
-    portBASE_TYPE taskAwoken = pdFALSE;
+    BaseType_t taskAwoken = pdFALSE;
 
     if (param->event == LEDC_FADE_END_EVT)
     {
-        ++g_fadeCompleteCount;
-
-        if (g_fadeCompleteCount >= 3)
-        {
-            // Only generate event when all LEDs are done fading
-            LEDFadeCompleteEvent ev{};
-            eventHandler.push_back([ev]() mutable { g_colorManager.onEvent(&ev); });
-            g_fadeCompleteCount = 0;
-        }
-        
+        SemaphoreHandle_t sem = (SemaphoreHandle_t) user_arg;
+        xSemaphoreGiveFromISR(sem, &taskAwoken);
     }
 
-    
-    return (taskAwoken == pdTRUE);
+    return taskAwoken;
 }
 
-
-LED &getLed()
+extern "C" void app_main(void)
 {
-    static ledc_cbs_t callbacks = {
-        .fade_cb = led_fade_handler
-    };
+    printf("Hello world!\n");
 
-    static LED led(&callbacks);
-    return led;
-}
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
 
-
-void app_main(void)
-{
-
-    /**
-     * Enable HW fade and register interrupt.
-     * Note that ledc_fade_func_install must come first, and also the SHARED flag needs to be set (docs sucks)
-     */
-    //ESP_ERROR_CHECK(ledc_fade_func_install(ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED));
-    //ESP_ERROR_CHECK(ledc_isr_register(led_isr_handler, NULL, ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED, NULL));
-
-    //ledc_set_fade_with_time(m_ledConf[RED].speed_mode, m_ledConf[RED].channel, 1023, 5000);
-    //ledc_fade_start(m_ledConf[RED].speed_mode, m_ledConf[RED].channel, LEDC_FADE_NO_WAIT);
-
-    
-
-    gpio_config_t btnConfig =
-    {
-        .pin_bit_mask = 1ULL << BTN,
-        .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_ENABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_NEGEDGE
-    };
-
-    gpio_config(&btnConfig);
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(BTN, gpio_isr_handler, NULL);
-
-    // TODO: initialize in constructor instead? RAII
-    g_debounceTimer.init();
-
-
-    auto red = std::unique_ptr<ColorMode>(new StaticColor(RGB(1000, 0, 0)));
-    auto yellow = std::unique_ptr<ColorMode>(new StaticColor(RGB(1000, 600, 0)));
-    auto green = std::unique_ptr<ColorMode>(new StaticColor(RGB(0, 1000, 0)));
-
-    Timer swTim(1500, timer_isr_handler, true, TIMER_1);
-    auto switchColors = std::vector<RGB>{ RGB(1000, 1000, 0), RGB(0, 1000, 1000), RGB(1000, 0, 1000) };
-    auto switchColor = std::unique_ptr<ColorMode>(new SwitchingColor(switchColors, swTim));
-
-    auto fadeColor = std::unique_ptr<ColorMode>(new FadingColor(RGB("b8ee30"), RGB("26dfd0"), swTim));
-
-    g_colorManager.addColorMode(move(red))
-        .addColorMode(move(fadeColor));
-        //.addColorMode(move(yellow))
-        //.addColorMode(move(green))
-        //.addColorMode(move(switchColor));
-    
-    g_colorManager.start();
-
-    while (true)
-    {
-        while (eventHandler.empty())
-        {
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-
-        auto &f = eventHandler.front();
-        f();
-
-        taskDISABLE_INTERRUPTS();
-        eventHandler.pop_front();
-        taskENABLE_INTERRUPTS();
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(nullptr, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
     }
+
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    gpio_config_t io_conf{};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = 1 << LEDPIN;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    //gpio_config(&io_conf);
+
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .duty_resolution = LEDC_TIMER_13_BIT,
+        .timer_num = LEDC_TIMER_0,
+        .freq_hz = 4000,
+        .clk_cfg = LEDC_AUTO_CLK,
+        .deconfigure = 0,
+    };
+
+    ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num = 16,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0,
+        .sleep_mode = LEDC_SLEEP_MODE_NO_ALIVE_NO_PD,
+        .flags = { .output_invert = 0 },
+    };
+
+    ledc_channel_config(&ledc_channel);
+
+    ledc_fade_func_install(0);
+    ledc_cbs_t callbacks = {
+        .fade_cb = cb_ledc_fade_end_event,
+    };
+
+    SemaphoreHandle_t ledSem = xSemaphoreCreateCounting(1, 1);
+
+    ledc_cb_register(ledc_channel.speed_mode, ledc_channel.channel, &callbacks, ledSem);
+    xSemaphoreTake(ledSem, portMAX_DELAY);
+    ledc_set_fade_time_and_start(ledc_channel.speed_mode, ledc_channel.channel, 8000, 2000, LEDC_FADE_NO_WAIT);
+
+    xSemaphoreTake(ledSem, portMAX_DELAY);
+    printf("start fading down...\n");
+    ledc_set_fade_time_and_start(ledc_channel.speed_mode, ledc_channel.channel, 0, 2000, LEDC_FADE_NO_WAIT);
+
+    while (1)
+    {
+        //gpio_set_level(LEDPIN, 1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        //gpio_set_level(LEDPIN, 0);
+        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 }
